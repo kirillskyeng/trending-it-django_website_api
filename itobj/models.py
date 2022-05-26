c2c435194dacab5720bf7e884daf295693f73782
kirill0720 <knete-87@mail.ru>
2022-05-26 15:03:11 +0300

    postgresql instead of sqlite3; ItObject model update

--- itobj/models.py
+++ itobj/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ItObject(models.Model):
+    title = models.CharField(max_length=80)
+    url = models.CharField(max_length=80)
+    content = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    trending = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
