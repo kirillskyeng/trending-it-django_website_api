0d9d6f3603de82420bcb6a9e38c6997001d182f7
kirill0720 <knete-87@mail.ru>
2022-05-27 12:19:43 +0300

    added template to show each post. reduced duplicated code in templates using inclusive tags

---
+++ itobj/templatetags/itobj_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from itobj.models import *
+
+register = template.Library()
+
+
+@register.inclusion_tag('itobj/show_sidebar.html')
+def show_sidebar():
+    cats = Category.objects.all()
+    return {'cats': cats}
+
+
+@register.inclusion_tag('itobj/show_title_stats.html')
+def show_title_stats(post=None):
+    return {'p': post}
--- itobj/views.py
+++ itobj/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
 menu = [{'title': 'About', 'url_name': 'about'},
         {'title': 'Add Post', 'url_name': 'add_page'},
-        {'title': 'Contact Us', 'url_name': 'contact'},
+        {'title': 'Contact', 'url_name': 'contact'},
         {'title': 'Register', 'url_name': 'register'},
         {'title': 'Login', 'url_name': 'login'},
         ]
@@ -13,12 +13,10 @@ menu = [{'title': 'About', 'url_name': 'about'},
 
 def index(request):
     posts = ItObject.objects.all()
-    cats = Category.objects.all()
     context = {
-        'title': 'Main Page',
+        'title': 'TrendingIT - trending, loved and interesting things in IT world.',
         'menu': menu,
         'posts': posts,
-        'cats': cats,
         'cat_selected': 0,
         'menu_active_url': 'home',
     }
@@ -27,20 +25,30 @@ def index(request):
 
 def show_category(request, cat_id):
     posts = ItObject.objects.filter(cat=cat_id)
-    cats = Category.objects.all()
     context = {
         'title': Category.objects.get(pk=cat_id),
         'menu': menu,
         'posts': posts,
-        'cats': cats,
         'cat_selected': cat_id,
         'menu_active_url': 'home',
     }
     return render(request, 'itobj/index.html', context=context)
 
 
-def show_post(request):
-    return HttpResponse('show_post Page')
+def show_post(request, post_id):
+    post = get_object_or_404(ItObject, pk=post_id)
+    cats = Category.objects.all()
+
+    context = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cats': cats,
+        'cat_selected': post.cat_id,
+        'menu_active_url': post.cat,
+    }
+
+    return render(request, 'itobj/post.html', context=context)
 
 
 def about(request):
